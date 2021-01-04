fae920f25dbb177acd69421c3ad5d282a410d4e5
Alan Lima <alan.lima123@hotmail.com>
2021-01-04 14:04:06 -0300

    GET /orders

--- backend/src/main/java/com/alanlima/dsdeliverer/repositories/OrderRepository.java
+++ backend/src/main/java/com/alanlima/dsdeliverer/repositories/OrderRepository.java
@@ -1,9 +1,15 @@
 package com.alanlima.dsdeliverer.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.alanlima.dsdeliverer.entities.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+	@Query("SELECT DISTINCT obj from Order obj JOIN FETCH obj.products "
+			+ " WHERE obj.status = 0 ORDER BY obj.moment ASC")
+	List<Order> findOrdersWithProducts();
 }
