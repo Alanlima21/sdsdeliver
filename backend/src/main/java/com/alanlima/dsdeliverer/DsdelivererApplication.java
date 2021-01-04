ad913455a7a4bbc8f6cabcbaa91031a81097ea26
Alan Lima <alan.lima123@hotmail.com>
2021-01-04 10:11:09 -0300

    Projeto criado

---
+++ backend/src/main/java/com/alanlima/dsdeliverer/DsdelivererApplication.java
@@ -0,0 +1,13 @@
+package com.alanlima.dsdeliverer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdelivererApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdelivererApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/alanlima/dsdeliverer/DsdelivererApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alanlima.dsdeliverer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdelivererApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
