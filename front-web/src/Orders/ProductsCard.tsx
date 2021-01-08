af373b257902c9ad64c0ae0f9293628f908889c1
Alan Lima <alan.lima123@hotmail.com>
2021-01-08 20:54:37 -0300

    finish front-web

--- front-web/src/Orders/ProductsCard.tsx
+++ front-web/src/Orders/ProductsCard.tsx
@@ -1,27 +1,21 @@
+import { formatPrice } from './helpers';
 import { Product } from './types';
 
 type Props = {
     product : Product;
-}
-
-function formatPrice(price : number){
-    const formatter = new Intl.NumberFormat('pt-br', {
-        style:'currency',
-        currency:'BRL'  
-    });
-
-    return formatter.format(price);
+    onSelectProduct: (product : Product) => void;
+    isSelected: boolean;
 }
 
 
-function ProductsCard({product}: Props){
+function ProductsCard({product, onSelectProduct, isSelected}: Props){
     return (
-       <div className="order-card-container">
+       <div className={`order-card-container ${isSelected ? 'selected': ''}`} onClick={() => onSelectProduct(product)}>
           <h3 className="order-card-title">
              {product.name}
           </h3>
             <img src={product.imageUri} alt={product.name} className="order-card-image"/>
-            <h3 className="order-card-price">{ formatPrice(product.price)}</h3>
+            <h3 className="order-card-price">{formatPrice(product.price)}</h3>
             <div className="order-card-description">
                 <h3>Descrição</h3>
                 <p>
